615fc3c94194f50c05677230948f73ffd610e73d
Josh Lyna <Josh.Lyna@sas.com>
2015-07-28 22:18:58 -0400

    Minor vimrc tweaks, furher break zshrc up

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -1,3 +1,19 @@
+# Antigen stuff
+
+source ~/.dotfiles/antigen/antigen.zsh
+
+antigen use oh-my-zsh
+
+antigen bundle zsh-users/zsh-syntax-highlighting
+antigen bundle sigurdga/ls-colors-solarized
+antigen bundle extract
+antigen bundle git-extras
+antigen bundle rupa/z
+
+antigen theme candy
+
+antigen apply
+
 # dircolors
 if [[ "$(tput colors)" == "256" ]]; then
     eval `dircolors ~/.antigen/repos/https-COLON--SLASH--SLASH-github.com-SLASH-sigurdga-SLASH-ls-colors-solarized.git/dircolors`
