0302b8d4b4465eaed854d7c937f52a955e05ed05
Josh Lyna <Josh.Lyna@sas.com>
2024-03-17 16:01:06 +0000

    Solarized dircolors

--- dot_zsh/plugins.zsh
+++ dot_zsh/plugins.zsh
@@ -1,26 +1,14 @@
-# Antigen stuff
-
-source ~/.dotfiles/antigen/antigen.zsh
-
-antigen use oh-my-zsh
-
-antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle sigurdga/ls-colors-solarized
-antigen bundle extract
-antigen bundle git-extras
-antigen bundle rupa/z
-antigen bundle mafredri/zsh-async
-antigen bundle sindresorhus/pure
-
-antigen apply
+# Antidote stuff
+source $(brew --prefix)/opt/antidote/share/antidote/antidote.zsh
+antidote load
 
 # dircolors
 if [[ "$(tput colors)" == "256" ]]; then
-    eval `dircolors /Users/jolyna/.antigen/bundles/sigurdga/ls-colors-solarized/dircolors`
+    eval `dircolors $HOME/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-sigurdga-SLASH-ls-colors-solarized/dircolors`
 fi
 
 # Base16 Shell
-BASE16_SHELL="$HOME/.config/base16-shell/"
-[ -n "$PS1" ] && \
-    [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
-        eval "$("$BASE16_SHELL/profile_helper.sh")"
+# BASE16_SHELL="$HOME/.config/base16-shell/"
+# [ -n "$PS1" ] && \
+#     [ -s "$BASE16_SHELL/profile_helper.sh" ] && \
+#         eval "$("$BASE16_SHELL/profile_helper.sh")"
