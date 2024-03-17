f612fff10fcbac9ed5b51bd168dc51e34b7fb580
Josh Lyna <Josh.Lyna@sas.com>
2024-03-17 04:15:11 +0000

    Fixup zsh aliases

--- dot_zsh/aliases.zsh
+++ dot_zsh/aliases.zsh
@@ -1,4 +1,7 @@
 # Use colors in coreutils utilities output
 alias ls='ls --color=auto'
+alias ll='ls --color=auto -lh'
+alias la='ls --color=auto -Ah'
+alias lal='ls --color=auto -lAh'
 alias grep='grep --color'
 alias k='kubectl'
